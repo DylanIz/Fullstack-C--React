0bf865b10617bb623a38e512d36d9705703e6ee4
DylanIz <dylandrumgoole2@hotmail.com>
2024-06-27 20:35:55 +0100

    Refactor search to app
    Move search to app, as data should be at the top of the application and events should be downstream

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
 
 function App() {
+  // useState is basically a get; set;
+  //<string> is explicitaly setting the type of the useState. We could of infered the state using ("")
+  const [search, setSearch] = useState<string>();
+  //using onClick is the equivalent of using a event listener in JS
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  }
+  //Hover over events to find the type
+  //Use SyntheticEvent type when having issues finding type of an event.
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  }
   return (
     <div className="App">
       <Search />
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -3,20 +3,6 @@ import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
 type Props = {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    // useState is basically a get; set;
-    //<string> is explicitaly setting the type of the useState. We could of infered the state using ("")
-    const [search, setSearch] = useState<string>();
-    //using onClick is the equivalent of using a event listener in JS
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
-    //Hover over events to find the type
-    //Use SyntheticEvent type when having issues finding type of an event.
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
-    }
-
     return (
         <div>
             <input value={search} onChange={(e) => handleChange(e)}></input>
