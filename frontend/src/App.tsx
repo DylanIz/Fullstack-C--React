7fcc25bf2496bb4c83658dd65373240f77a4754c
DylanIz <dylandrumgoole2@hotmail.com>
2024-06-27 21:03:30 +0100

    Adding type narrowing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
   // useState is basically a get; set;
   //<string> is explicitaly setting the type of the useState. We could of infered the state using ("")
-  const [search, setSearch] = useState<string>();
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
   //using onClick is the equivalent of using a event listener in JS
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -14,12 +18,17 @@ function App() {
   }
   //Hover over events to find the type
   //Use SyntheticEvent type when having issues finding type of an event.
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if(typeof result === "string"){
+      setServerError(result);
+    } else if(Array.isArray(result.data)){
+      setSearchResult(result.data)
+    }
   }
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
     </div>
   );
