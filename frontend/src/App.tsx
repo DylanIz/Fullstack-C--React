854e26e55fbc2c727c235953c801e10b4810e2ec
DylanIz <dylandrumgoole2@hotmail.com>
2024-07-30 18:57:01 +0100

    Refactor search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,54 @@
-import { ChangeEvent, SyntheticEvent, useState } from 'react';
-import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import "./App.css";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
+import { CompanySearch } from "./company";
+import { searchCompanies } from "./api";
 
 function App() {
-  // useState is basically a get; set;
-  //<string> is explicitaly setting the type of the useState. We could of infered the state using ("")
   const [search, setSearch] = useState<string>("");
   const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
-  //using onClick is the equivalent of using a event listener in JS
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
+
+  const onPortfolioCreate = (e: SyntheticEvent) => {
+    e.preventDefault();
     console.log(e);
-  }
-  //Hover over events to find the type
-  //Use SyntheticEvent type when having issues finding type of an event.
-  const onClick = async (e: SyntheticEvent) => {
+  };
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
     const result = await searchCompanies(search);
-    if(typeof result === "string"){
+    if (typeof result === "string") {
       setServerError(result);
-    } else if(Array.isArray(result.data)){
-      setSearchResult(result.data)
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
     }
-  }
-  //Added conditional rendering of serverError for when response is a string. For example a network error.
+  };
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList searchResults={searchResult}/>
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
       {serverError && <h1>{serverError}</h1>}
     </div>
   );
 }
 
 export default App;
+
+// useState is basically a get; set;
+//<string> is explicitaly setting the type of the useState. We could of infered the state using ("")
+//using onClick is the equivalent of using a event listener in JS
+//Hover over events to find the type
+//Use SyntheticEvent type when having issues finding type of an event.
+//Added conditional rendering of serverError for when response is a string. For example a network error.
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,18 +1,23 @@
-import React, { ChangeEvent, useState, SyntheticEvent, FormEvent } from 'react'
+import React, { ChangeEvent, useState, SyntheticEvent, FormEvent } from "react";
 
 interface Props {
-    onClick: (e: SyntheticEvent) => void;
-    search: string | undefined;
-    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Search: React.FC<Props> = ({ onClick, search, handleChange }: Props): JSX.Element => {
-    return (
-        <div>
-            <input value={search} onChange={(e) => handleChange(e)}></input>
-            <button onClick={(e) => onClick(e)}></button>
-        </div>
-    )
-}
+const Search: React.FC<Props> = ({
+  onSearchSubmit,
+  search,
+  handleSearchChange,
+}: Props): JSX.Element => {
+  return (
+    <>
+      <form onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange}></input>
+      </form>
+    </>
+  );
+};
 
-export default Search
+export default Search;
