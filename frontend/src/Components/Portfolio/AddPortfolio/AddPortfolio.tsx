434482f9344abe427d12bfd20599b2766a8e37ec
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-27 19:55:33 +0000

    Aug 27, 2024, 8:55 PM

--- frontend/src/Components/Portfolio/AddPortfolio/AddPortfolio.tsx
+++ frontend/src/Components/Portfolio/AddPortfolio/AddPortfolio.tsx
@@ -1,15 +1,22 @@
-import React, { SyntheticEvent } from 'react'
+import React, { SyntheticEvent } from "react";
 
 interface Props {
-    onPortfolioCreate: (e:SyntheticEvent) => void;
-    symbol: string
+  onPortfolioCreate: (e: SyntheticEvent) => void;
+  symbol: string;
 }
 
-const AddPortfolio = ({onPortfolioCreate, symbol}: Props) => {
-  return <form onSubmit={onPortfolioCreate}>
-    <input readOnly hidden value={symbol}></input>
-    <button type="submit">Add</button>
-  </form>
-}
+const AddPortfolio = ({ onPortfolioCreate, symbol }: Props) => {
+  return (
+    <form onSubmit={onPortfolioCreate}>
+      <input readOnly hidden value={symbol}></input>
+      <button
+        type="submit"
+        className="p-2 px-8 text-white bg-darkBlue rounded-lg hover:opacity-70 focus:outline-none"
+      >
+        Add
+      </button>
+    </form>
+  );
+};
 
-export default AddPortfolio
+export default AddPortfolio;
