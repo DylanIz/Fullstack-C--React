47205024c69f1117d8e283668918c36ff7156efe
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-27 20:41:51 +0000

    Aug 27, 2024, 9:41 PM

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { ChangeEvent, SyntheticEvent, useState } from "react";
+import { searchCompanies } from "../../api";
+import { CompanySearch } from "../../company";
+import CardList from "../../Components/CardList/CardList";
+import Hero from "../../Components/Hero/Hero";
+import Navbar from "../../Components/Navbar/Navbar";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import Search from "../../Components/Search/Search";
 
 interface Props {}
 
 const SearchPage = (props: Props) => {
-  return <div>SearchPage</div>;
+  const [search, setSearch] = useState<string>("");
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+  };
+
+  const onPortfolioDelete = (e: any) => {
+    e.preventDefault();
+    const filteredPortfolio = portfolioValues.filter((value) => {
+      return value !== e.target[0].value;
+    });
+    setPortfolioValues(filteredPortfolio);
+  };
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+  };
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Search
+        onSearchSubmit={onSearchSubmit}
+        search={search}
+        handleSearchChange={handleSearchChange}
+      />
+      <ListPortfolio
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
+      {serverError && <h1>{serverError}</h1>}
+    </div>
+  );
 };
 
 export default SearchPage;
