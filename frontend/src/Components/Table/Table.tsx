67490c2368beb7c368b07b12d37a36bc1539575a
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 19:41:06 +0000

    Aug 28, 2024, 8:41 PM

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { testIncomeStatementData } from "./testData";
+
+const data = testIncomeStatementData;
 
 interface Props {}
+
+type Company = (typeof data)[0];
+
+const configs = [
+  {
+    label: "Year",
+    render: (company: Company) => company.acceptedDate,
+  },
+  {
+    label: "Cost of Revenue",
+    render: (company: Company) => company.costOfRevenue,
+  },
+];
 
 function Table({}: Props) {
   return <div>Table</div>;
