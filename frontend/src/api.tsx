a97c0120ce2c70953087d3f980893f692a287b34
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 17:43:07 +0000

    Aug 28, 2024, 6:43 PM

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,21 +1,29 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
 export const searchCompanies = async (query: string) => {
-    try {
-        const data = await axios.get<SearchResponse>(`https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`);
-        return data;
-    } catch (error) {
-        if(axios.isAxiosError(error)){
-            console.log("Axios error: ", error.message);
-            return error.message;
-        } else {
-            console.log("Unexpected error: ", error)
-            return "An unexpected error has occured.";
-        }
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`,
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("Axios error: ", error.message);
+      return error.message;
+    } else {
+      console.log("Unexpected error: ", error);
+      return "An unexpected error has occured.";
     }
-}
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>;
+  } catch {}
+};
