5be8ef59e6360c40dbf5160d2a975ace9b53346d
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 18:53:21 +0000

    Aug 28, 2024, 7:53 PM

--- frontend/src/Components/CompanyDashboard/CompanyDashboard.tsx
+++ frontend/src/Components/CompanyDashboard/CompanyDashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tile from "../Tile/Tile";
 
 interface Props {}
 
@@ -8,7 +9,9 @@ function CompanyDashboard({}: Props) {
       <div className="relative pt-20 pb-32 bg-lightBlue-500">
         <div className="px-4 md:px-6 mx-auto w-full">
           <div>
-            <div className="flex flex-wrap"></div>
+            <div className="flex flex-wrap">
+              <Tile title={"Tsla"} subTitle={"Tsla Info"} />
+            </div>
           </div>
         </div>
       </div>
