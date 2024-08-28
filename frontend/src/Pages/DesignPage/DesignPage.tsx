b6168019e311052afa854c76e7675a3b71fb511a
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 19:08:10 +0000

    Aug 28, 2024, 8:08 PM

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>Finance Design Page</h1>
+    </>
+  );
+};
+
+export default DesignPage;
