5b4202de06e52f016e478e1e5102ed3a1fd54dc9
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 17:49:46 +0000

    Aug 28, 2024, 6:49 PM

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+  });
   return <div>CompanyPage</div>;
 };
 
