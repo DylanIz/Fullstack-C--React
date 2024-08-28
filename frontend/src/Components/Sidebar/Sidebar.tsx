add7fc75cddaddf57bca4eeae33237428c12668a
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 18:23:45 +0000

    Aug 28, 2024, 7:23 PM

--- frontend/src/Components/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Sidebar/Sidebar.tsx
@@ -19,6 +19,7 @@ function Sidebar({}: Props) {
               className="flex md:min-w-full text-blueGray-500 text-base uppercase font-bold block pt-1 pb-4 no-underline"
             >
               <FaHome />
+              <h6 className="ml-3">Company Profile</h6>
             </Link>
           </div>
         </div>
