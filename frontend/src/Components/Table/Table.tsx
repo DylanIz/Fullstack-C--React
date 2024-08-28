c9407684dbd1c7db166c84e4de3ce6ab8e55bf1d
DylanIz <dylandrumgoole2@hotmail.com>
2024-08-28 19:48:14 +0000

    Aug 28, 2024, 8:48 PM

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -19,7 +19,36 @@ const configs = [
 ];
 
 function Table({}: Props) {
-  return <div>Table</div>;
+  const renderedRow = data.map((company) => {
+    return (
+      <tr key={company.cik}>
+        {configs.map((val: any) => {
+          return (
+            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
+              {val.render(company)}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  });
+  const renderedHeaders = configs.map((config: any) => {
+    return (
+      <th
+        className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        key={config.label}
+      >
+        {config.label}
+      </th>
+    );
+  });
+  return (
+    <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+      <table>
+        <thead className=""></thead>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
