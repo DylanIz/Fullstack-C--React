b39093027e416a64f3efbef79c4bd887850b81fc
DylanIz <dylandrumgoole2@hotmail.com>
2024-09-16 17:52:57 +0000

    Sep 16, 2024, 6:52 PM

--- frontend/src/Components/RatioList/RatioList.tsx
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -17,7 +17,17 @@ const config = [
 
 const RatioList = (props: Props) => {
   const renderedRow = config.map((row) => {
-    return <li className="py-3 sm:py-4">.flex.</li>;
+    return (
+      <li className="py-3 sm:py-4">
+        <div className="flex items-cener space-x-4">
+          <div className="flex-1 min-4">
+            <p className="text-sm font-medium text-gray-900 truncate">
+              {row.label}
+            </p>
+          </div>
+        </div>
+      </li>
+    );
   });
   return <div>RatioList</div>;
 };
