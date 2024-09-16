e7492c8beaf4a43067fa1ff5484df65bb8d13c48
DylanIz <dylandrumgoole2@hotmail.com>
2024-09-16 17:48:11 +0000

    Sep 16, 2024, 6:48 PM

--- frontend/src/Components/RatioList/RatioList.tsx
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -3,7 +3,17 @@ import { TestDataCompany } from "../Table/testData";
 
 interface Props {}
 
-const data = TestDataCompany;
+const data = TestDataCompany[0];
+
+type Company = typeof data;
+
+const config = [
+  {
+    label: "Company CEO",
+    render: (company: Company) => company.ceo,
+    subTitle: "This is the company CEO",
+  },
+];
 
 const RatioList = (props: Props) => {
   return <div>RatioList</div>;
