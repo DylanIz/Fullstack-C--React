92935079bbc349ec1d148edec6a02c89dd2887d7
DylanIz <dylandrumgoole2@hotmail.com>
2024-09-16 18:34:40 +0000

    Sep 16, 2024, 7:34 PM

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -36,10 +36,10 @@ export const getCompanyProfile = async (query: string) => {
 export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
-      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`,
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?apikey=${process.env.REACT_APP_API_KEY}`,
     );
     return data;
   } catch (error: any) {
-    throw error("Error fetching profile: ", error.message);
+    throw error("Error fetching Key Metrics: ", error.message);
   }
 };
