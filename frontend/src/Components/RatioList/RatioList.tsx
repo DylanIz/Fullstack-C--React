81c59829bd811c96bcd7847c226b385a9c366649
DylanIz <dylandrumgoole2@hotmail.com>
2024-09-16 17:45:24 +0000

    Sep 16, 2024, 6:45 PM

---
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TestDataCompany } from "../Table/testData";
+
+interface Props {}
+
+const data = TestDataCompany;
+
+const RatioList = (props: Props) => {
+  return <div>RatioList</div>;
+};
+
+export default RatioList;
