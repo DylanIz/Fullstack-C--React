6ae2218216440a66ef1c688bf0a0052b4104574a
DylanIz <dylandrumgoole2@hotmail.com>
2024-11-18 18:38:50 +0000

    Nov 18, 2024, 6:38 PM

--- frontend/src/Components/Spinner/Spinner.tsx
+++ frontend/src/Components/Spinner/Spinner.tsx
@@ -9,7 +9,7 @@ const Spinner = ({ isLoading = true }: Props) => {
   return (
     <>
       <div id="loading-spinner">
-        <ClipLoader color="#36d7b7" />
+        <ClipLoader color="#36d7b7" loading={isLoading} />
       </div>
     </>
   );
