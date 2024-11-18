b61f7632531fb97a1f5cf1eaf4ec1db7233fc1b2
DylanIz <dylandrumgoole2@hotmail.com>
2024-11-18 19:28:57 +0000

    Nov 18, 2024, 7:28 PM

--- frontend/src/Components/TenKFinder/TenKFinder.tsx
+++ frontend/src/Components/TenKFinder/TenKFinder.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { CompanyTenK } from "../../company";
+import { getTenK } from "../../api";
 
 interface Props {
-    ticker: string;
+  ticker: string;
 }
 
-const TenKFinder = (props: Props) => {
-    const 
+const TenKFinder = ({ ticker }: Props) => {
+  const [companyData, setCompanyData] = React.useState<CompanyTenK[]>([]);
+  useEffect(() => {
+    const getTenKData = async () => {
+      const value = await getTenK(ticker);
+      if (value?.data) {
+        setCompanyData(value.data);
+      }
+    };
+    getTenKData();
+  }, [ticker]);
+
   return <div>TenKFinder</div>;
 };
 
