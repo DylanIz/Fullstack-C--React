8397db99488890cd5a6cfaf3842747f3eee9b033
DylanIz <dylandrumgoole2@hotmail.com>
2024-11-18 19:26:34 +0000

    Nov 18, 2024, 7:26 PM

---
+++ frontend/src/Components/TenKFinder/TenKFinder.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+    ticker: string;
+}
+
+const TenKFinder = (props: Props) => {
+    const 
+  return <div>TenKFinder</div>;
+};
+
+export default TenKFinder;
