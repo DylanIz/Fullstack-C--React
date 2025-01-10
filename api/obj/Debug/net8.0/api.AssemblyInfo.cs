ae458ee91daaef601ff0e3c240da0c3c0546e761
DylanIz <dylandrumgoole2@hotmail.com>
2025-01-10 22:00:30 +0000

    Jan 10, 2025, 10:00 PM

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8524201fe2680e41543996bec625db62ac175e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c99fb68d64ea14312a40a5df348ef32cafe2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
