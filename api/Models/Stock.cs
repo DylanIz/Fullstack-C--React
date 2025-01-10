8f6dd5e6e476b2e5ec810b9bb42b3d3f781bbc5d
DylanIz <dylandrumgoole2@hotmail.com>
2025-01-10 22:05:42 +0000

    Jan 10, 2025, 10:05 PM

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -1,13 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
 {
     public class Stock
     {
         public int Id { get; set; }
-        public string Symbol { get; set; }  
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Dividend { get; set; }
     }
 }
