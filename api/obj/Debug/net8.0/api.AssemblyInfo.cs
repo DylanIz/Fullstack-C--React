a30b58948d059f5c3cb753aa54db7a2b2c66ddd7
DylanIz <dylandrumgoole2@hotmail.com>
2025-01-13 20:25:07 +0000

    Adding DTO/Mappers

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80c6f3cccec04b8eb4caf701d977e7c4f347d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8acfbb33c5d0e412dec35445a24c733d63cdc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
